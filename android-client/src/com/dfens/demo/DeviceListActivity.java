6b1d215f4fbb0d8b98d3413e1f3735e3c841c3eb
Mattias Rosberg <mattias.rosberg@jayway.com>
2014-11-11 22:51:45 +0100

    Added settings mode in DeviceListActivity

--- android-client/src/com/dfens/demo/DeviceListActivity.java
+++ android-client/src/com/dfens/demo/DeviceListActivity.java
@@ -7,12 +7,11 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Switch;
 import com.dfens.demo.domain.DfensEvent;
 import com.dfens.demo.network.NetClient;
-import org.androidannotations.annotations.Bean;
-import org.androidannotations.annotations.EActivity;
-import org.androidannotations.annotations.Extra;
-import org.androidannotations.annotations.NoTitle;
+import org.androidannotations.annotations.*;
 
 /**
  * Created by mattias on 2014-11-02.
@@ -21,6 +20,33 @@ import org.androidannotations.annotations.NoTitle;
 @NoTitle
 @EActivity(R.layout.activity_device_list)
 public class DeviceListActivity extends Activity {
+
+    @ViewById(R.id.switch_1)
+    Switch switch1;
+
+    @ViewById(R.id.switch_2)
+    Switch switch2;
+
+    @ViewById(R.id.switch_3)
+    Switch switch3;
+
+    @ViewById(R.id.switch_4)
+    Switch switch4;
+
+    @ViewById(R.id.switch_5)
+    Switch switch5;
+
+    @ViewById(R.id.switch_6)
+    Switch switch6;
+
+    @ViewById(R.id.switch_7)
+    Switch switch7;
+
+    @ViewById(R.id.switch_8)
+    Switch switch8;
+
+    @ViewById(R.id.switch_9)
+    Switch switch9;
 
     @Bean
     NetClient netClient;
@@ -105,5 +131,18 @@ public class DeviceListActivity extends Activity {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    @Click(R.id.settings_image)
+    public void onClickSettingsImage(View button) {
+        switch1.setVisibility(switch1.getVisibility() == View.VISIBLE ? View.GONE : View.VISIBLE);
+        switch2.setVisibility(switch2.getVisibility() == View.VISIBLE ? View.GONE : View.VISIBLE);
+        switch3.setVisibility(switch3.getVisibility() == View.VISIBLE ? View.GONE : View.VISIBLE);
+        switch4.setVisibility(switch4.getVisibility() == View.VISIBLE ? View.GONE : View.VISIBLE);
+        switch5.setVisibility(switch5.getVisibility() == View.VISIBLE ? View.GONE : View.VISIBLE);
+        switch6.setVisibility(switch6.getVisibility() == View.VISIBLE ? View.GONE : View.VISIBLE);
+        switch7.setVisibility(switch7.getVisibility() == View.VISIBLE ? View.GONE : View.VISIBLE);
+        switch8.setVisibility(switch8.getVisibility() == View.VISIBLE ? View.GONE : View.VISIBLE);
+        switch9.setVisibility(switch9.getVisibility() == View.VISIBLE ? View.GONE : View.VISIBLE);
     }
 }
